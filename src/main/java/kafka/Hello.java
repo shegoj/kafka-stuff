342ca72a8534778e87d6402356300d01cfe9aa0a
shegoj <shegoj@yahoo.com>
2023-01-12 12:22:04 +0000

    hello

---
+++ src/main/java/kafka/Hello.java
@@ -0,0 +1,65 @@
+package kafka;
+
+import org.apache.kafka.common.serialization.Serde;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.*;
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import java.util.Properties;
+import java.util.concurrent.CountDownLatch;
+
+public class Hello {
+    public static void main(String[] args) {
+
+
+        BasicConfigurator.configure();
+        Logger.getRootLogger().setLevel(Level.INFO);
+
+        final Serde<String> stringSerde = Serdes.String();
+        final Serde<Long> longSerde = Serdes.Long();
+
+        final StreamsBuilder builder = new StreamsBuilder();
+
+        KStream<String, String> views = builder.stream(
+               args[0],
+                Consumed.with(stringSerde, stringSerde)
+        );
+
+        KTable<String, Long> totalViews = views
+                .mapValues(v -> Long.parseLong(v))
+                .groupByKey(Grouped.with(stringSerde, longSerde))
+                .reduce(Long::sum);
+
+        totalViews.toStream().to("output-topic", Produced.with(stringSerde, longSerde));
+
+        final Properties props = new Properties();
+        props.putIfAbsent(StreamsConfig.APPLICATION_ID_CONFIG,      args[1]);
+        props.putIfAbsent(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG,      args[2]);
+
+        final KafkaStreams streams = new KafkaStreams(builder.build(), props);
+
+        final CountDownLatch latch = new CountDownLatch(1);
+
+        try {
+            streams.start();
+            latch.await();
+        } catch (final Throwable e) {
+            System.exit(1);
+        }
+
+        Runtime.getRuntime().addShutdownHook(new Thread("streams-totalviews") {
+            @Override
+            public void run() {
+                streams.close();
+                latch.countDown();
+            }
+        });
+
+        System.exit(0);
+    }
+}
+
